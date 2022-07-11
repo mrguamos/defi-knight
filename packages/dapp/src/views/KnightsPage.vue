b7892dd3ae5fff934c1991a82f881672f62e299e
Mark Ryan Guamos <ryan.guamos@gmail.com>
2022-07-11 13:22:51 +0800

    ui fixes

--- packages/dapp/src/views/KnightsPage.vue
+++ packages/dapp/src/views/KnightsPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col h-full pb-10 pt-28">
+  <div class="flex flex-col h-full pb-10 pt-28 px-5">
     <NFTList
       :items="paginatedKnights"
       nft="knights"
